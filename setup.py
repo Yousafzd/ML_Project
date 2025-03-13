7ac4ac3eab8fa38ffd324d8cf0c7c9f74858fdc4
Md-Yousaf <yousafzadran50@gmail.com>
2025-03-13 06:18:00 +0500

    setup and requirments is done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name='ML_Project',
+    version='0.0.1',
+    author='Yousafzd',
+    author_email='yousafzadran50@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+    
+)
